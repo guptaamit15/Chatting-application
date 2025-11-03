f9161880ca46522da3faf47db6615b8753920050
Amit Gupta <93533885+guptaamit15@users.noreply.github.com>
2025-11-03 03:26:56 -0500

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black text-white">
+      Hello Amit
+    </div>
+  );
+}
+
+export default App;
